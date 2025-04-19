f2ba5911f8749598e6a59f8ac35465da73ff7f14
Muhammad Aziz Habiburrahim <125756197+mhabibr02@users.noreply.github.com>
2025-04-19 16:57:46 +0700

    Create RefactorCode.py

---
+++ Natural Language/RefactorCode.py
@@ -0,0 +1,17 @@
+def calculateTotalPrice(item, quantity):
+
+    if item == 'apple':
+
+        return quantity * 0.5
+
+    elif item == 'banana':
+
+        return quantity * 0.75
+
+    elif item == 'orange':
+
+        return quantity * 0.6
+
+    else:
+
+        return 0
