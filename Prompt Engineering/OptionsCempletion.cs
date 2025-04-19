d46593f13f4c5e83cb4614dbcfe60082f30bb3e5
Muhammad Aziz Habiburrahim <125756197+mhabibr02@users.noreply.github.com>
2025-04-19 16:46:16 +0700

    Create OptionsCempletion.cs

---
+++ Prompt Engineering/OptionsCempletion.cs
@@ -0,0 +1,25 @@
+{
+
+    Messages =
+
+    {
+
+        new ChatRequestSystemMessage("You are a helpful assistant."),
+
+        new ChatRequestUserMessage("That was an awesome experience"),
+
+        new ChatRequestAssistantMessage("positive"),
+
+        new ChatRequestUserMessage("I won't do that again"),
+
+        new ChatRequestAssistantMessage("negative"),
+
+        new ChatRequestUserMessage("That was not worth my time"),
+
+        new ChatRequestAssistantMessage("negative"),
+
+        new ChatRequestUserMessage("You can't miss this")
+
+    }
+
+};
