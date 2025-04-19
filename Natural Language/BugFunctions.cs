4f857f195c576953d20d4caf1e63e2f5b8944df9
Muhammad Aziz Habiburrahim <125756197+mhabibr02@users.noreply.github.com>
2025-04-19 16:55:08 +0700

    Create BugFunctions.cs

---
+++ Natural Language/BugFunctions.cs
@@ -0,0 +1,21 @@
+public double CalculateAverage(int[] numbers)
+
+{
+
+int total = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+
+{
+
+        int number = numbers[i];
+
+        total += number;
+
+}
+
+double average = (double)total / numbers.Length;
+
+return average;
+
+}
